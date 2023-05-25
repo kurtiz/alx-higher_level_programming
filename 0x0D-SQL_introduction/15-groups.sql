4f7a56452795d75395f7e49add8f77bac3544299
Aaron Will Djaba <papiliocurtis@gmail.com>
2023-05-25 07:18:12 +0000

    fixed some minor issues :hammer:

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
 -- counts and shows
-SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
